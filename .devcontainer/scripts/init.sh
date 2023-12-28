c07a53e0074ec7e6e125b6ac8e230a38748c31e4
윤용식 <comnori@gmail.com>
2023-12-28 16:22:04 +0900

    Update devcontainer.json and init.sh

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,15 +1,21 @@
 #/bin/sh
 
+sh -c "git config --global init.defaultBranch main"
+
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" -- \
   -t jonathan \
   -p git \
   -p ssh-agent \
   -p https://github.com/zsh-users/zsh-autosuggestions \
-  -p https://github.com/zsh-users/zsh-completions
+  -p https://github.com/zsh-users/zsh-completions \
+  -p https://github.com/zsh-users/zsh-syntax-highlighting \
+  -x
 
-corepack enable
+sh -c "corepack enable"
 
-yarn global add serve
+sh -c "yarn config set --home enableTelemetry 0"
+
+sh -c "yarn global add serve"
 
 cat << EOF >> ~/.zshrc
 
@@ -18,3 +24,5 @@ if [ -d "\$HOME/.yarn/bin" ] ; then
   PATH="\$(yarn global bin):\$PATH"
 fi
 EOF
+
+sh -c "yarn install"
